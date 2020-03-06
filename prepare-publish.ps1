1bff27dcd10ccfc5c82fc58d3013e5d994fac2fb
F2 <916098+F2@users.noreply.github.com>
2020-03-06 22:20:56 +0100

    Added prepare-publish.ps1

---
+++ prepare-publish.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference = "Stop"
+
+function ZipFiles($zipfilename, $sourcedir)
+{
+   Add-Type -Assembly System.IO.Compression.FileSystem
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $zipfilename, $compressionLevel, $false)
+}
+
+if (Test-Path dist) {
+    Get-ChildItem -Path "dist" -Recurse | Remove-Item -force -recurse
+}
+
+New-Item -ItemType directory -Path dist\release
+New-Item -ItemType directory -Path dist\source\includes
+New-Item -ItemType directory -Path dist\ftp
+
+$plugins = @("waitforstv", "medicstats", "supstats2", "logstf", "restorescore", "fixstvslot", "pause", "recordstv", "classwarning", "afk");
+
+foreach ($p in $plugins) {
+    Write-Host "Compiling $p..."
+    & "spcomp" "$p/$p.sp" "-i" "$PSScriptRoot\includes" "-w217"
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed compilation of $p"
+        Exit 1
+    }
+}
+
+Write-Host "Finished successfully"
