fa0daa508fa8cd6f9c94638a6690906eb21ef45d
F2 <916098+F2@users.noreply.github.com>
2020-03-07 10:07:16 +0100

    Updated publish

--- prepare-publish.ps1
+++ prepare-publish.ps1
@@ -7,25 +7,58 @@ function ZipFiles($zipfilename, $sourcedir)
    [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $zipfilename, $compressionLevel, $false)
 }
 
-if (Test-Path dist) {
-    Get-ChildItem -Path "dist" -Recurse | Remove-Item -force -recurse
+function ZipFile($zipfilename, $sourcefile)
+{
+    $sourceDir = Split-Path (Resolve-Path $sourcefile)
+    $tempDir = Join-Path $sourceDir "tempZipDir/"
+    New-Item $tempDir -ItemType Directory
+    Copy-Item $sourcefile $tempDir
+    ZipFiles $zipfilename $tempDir
+    Write-Host "Want to remove: $($tempDir)"
+    Remove-Item (Join-Path $tempDir (Split-Path -Leaf $sourcefile))
+    Remove-Item $tempDir
 }
 
-New-Item -ItemType directory -Path dist\release
-New-Item -ItemType directory -Path dist\source\includes
-New-Item -ItemType directory -Path dist\ftp
+if (Test-Path dist) {
+    Get-ChildItem -Path "dist" -Recurse | Remove-Item -Force -Recurse
+}
 
-$plugins = @("waitforstv", "medicstats", "supstats2", "logstf", "restorescore", "fixstvslot", "pause", "recordstv", "classwarning", "afk");
+New-Item -ItemType directory -Path (Join-Path "dist" "release")
+New-Item -ItemType directory -Path (Join-Path "dist" "source")
+New-Item -ItemType directory -Path (Join-Path "dist" "ftp")
+Copy-Item -Path "includes" -Destination (Join-Path "dist" "source") -Recurse
+
+
+$plugins = @("waitforstv", "medicstats", "supstats2", "logstf", "restorescore", "countdown", "fixstvslot", "pause", "recordstv", "classwarning", "afk");
 
 foreach ($p in $plugins) {
     Write-Host "Compiling $p..."
-    Write-Host "Command: & spcomp $p/$p.sp -i $(Join-Path $PSScriptRoot includes) -w217"
-    & "spcomp" "$p/$p.sp" "-i" (Join-Path $PSScriptRoot includes) "-w217"
+    & "spcomp" (Join-Path $p "$p.sp") "-i" (Join-Path $PSScriptRoot includes) "-w217"
 
     if ($LASTEXITCODE -ne 0) {
         Write-Host "Failed compilation of $p"
         Exit 1
     }
+
+    # Copy the .smx and update.txt file to UPDATE
+    New-Item -ItemType directory -Path (Join-Path "dist" "ftp\$p\plugins")
+    Copy-Item -Path (Join-Path $p "update.txt") -Destination (Join-Path "dist" "ftp\$p")
+    Copy-Item -Path (Join-Path $p "$p.smx") -Destination (Join-Path "dist" "ftp\$p\plugins")
+
+    # Copy the .smx file to the common RELEASE directory
+    Copy-Item -Path (Join-Path $p "$p.smx") -Destination (Join-Path "dist" "release")
+
+    # Copy the .sp file to the common SOURCE directory
+    Copy-Item -Path (Join-Path $p "$p.sp") -Destination (Join-Path "dist" "source")
+
+    # Zip the single smx file
+    ZipFile (Join-Path "dist" "ftp\$p.zip") (Join-Path $p "$p.smx")
 }
+
+# Zip the common RELEASE directory
+ZipFiles (Join-Path "dist" "ftp\f2-sourcemod-plugins.zip") (Join-Path "dist" "release")
+
+# Zip the common SOURCE directory
+ZipFiles (Join-Path "dist" "ftp\f2-sourcemod-plugins-src.zip") (Join-Path "dist" "source")
 
 Write-Host "Finished successfully"
