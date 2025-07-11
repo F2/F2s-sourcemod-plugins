b296b7087bd7383d3fdba5ba5bf02b2ac9449a8d
F2 <916098+F2@users.noreply.github.com>
2025-07-11 16:44:24 +0200

    Use SM 1.12 for compilation

--- setup.ps1
+++ setup.ps1
@@ -9,21 +9,38 @@ $env:PATH += [IO.Path]::PathSeparator + $PSScriptRoot + [IO.Path]::PathSeparator
 $env:PATH += Join-Path $PSScriptRoot "sourcemod-binaries" "addons" "sourcemod" "scripting"
 Write-Host "DONE!"
 
+function Get-SpcompVersion {
+    try {
+        $spcompVersionString = (& "spcomp" "--version") | Select-Object -First 1
+        if ($spcompVersionString -match "SourcePawn Compiler (\d+\.\d+)(\.\d+)*") {
+            return $Matches[1]
+        }
+    }
+    catch {
+    }
+    return ""
+}
+
 Write-Host -NoNewline "Installing SourceMod binaries... "
-if (Test-Path "sourcemod-binaries") {
+if ((Test-Path "sourcemod-binaries") -and (Get-SpcompVersion) -eq "1.12") {
     Write-Host "already installed."
 }
 else {
+    if (Test-Path "sourcemod-binaries") {
+        $timestamp = (Get-Date).ToString("yyyyMMdd_HHmmss")
+        Rename-Item "sourcemod-binaries" ("sourcemod-binaries-backup-" + $timestamp)
+    }
+
     if ($IsWindows) {
-        $sourcemodFilename = "sourcemod-1.10.0-git6545-windows.zip"
-        $sourcemodUrl = "https://sm.alliedmods.net/smdrop/1.10/" + $sourcemodFilename
+        $sourcemodFilename = "sourcemod-1.12.0-git7210-windows.zip"
+        $sourcemodUrl = "https://sm.alliedmods.net/smdrop/1.12/" + $sourcemodFilename
         Invoke-WebRequest $sourcemodUrl -OutFile $sourcemodFilename
         Expand-Archive $sourcemodFilename -DestinationPath "sourcemod-binaries"
         Remove-Item $sourcemodFilename
     }
     elseif ($IsLinux) {
-        $sourcemodFilename = "sourcemod-1.10.0-git6545-linux.tar.gz"
-        $sourcemodUrl = "https://sm.alliedmods.net/smdrop/1.10/" + $sourcemodFilename
+        $sourcemodFilename = "sourcemod-1.12.0-git7210-linux.tar.gz"
+        $sourcemodUrl = "https://sm.alliedmods.net/smdrop/1.12/" + $sourcemodFilename
         Invoke-WebRequest $sourcemodUrl -OutFile $sourcemodFilename
         mkdir sourcemod-binaries
         if (!$?) { exit $LASTEXITCODE }
